3407c6d989131a629bfe44a7abe906a9bbaf861e
Jeremie Shaker <jeremie.shaker@hotmail.com>
2016-12-16 23:41:36 -0500

    emptying out placeholder code

--- app/App.js
+++ app/App.js
@@ -16,14 +16,7 @@ export default class App extends Component {
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit App.js
-                </Text>
-                <Text style={styles.instructions}>
-                    Double tap R on your keyboard to reload,{'\n'}
-                    Shake or press menu button for dev menu
+                    OddZ
                 </Text>
             </View>
         );
@@ -33,18 +26,11 @@ export default class App extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        backgroundColor: '#F5FCFF'
     },
     welcome: {
         fontSize: 20,
         textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
+        margin: 10
+    }
 });
