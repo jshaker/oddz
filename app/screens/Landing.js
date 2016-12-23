c9c94cd1c2330ba4274e4753ff5bbb1e9ed68ea9
Mathieu Diab <diabmathieu@gmail.com>
2016-12-23 11:01:44 -0500

    added signup and login functionality

---
+++ app/screens/Landing.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+
+export default class Landing extends Component{
+  constructor(props){
+    super(props)
+    this.state{
+
+    }
+  }
+  render(){
+    return(
+      <View>
+        Logged In
+      </View>
+    )
+  }
+}
--- app/screens/Login.js
+++ app/screens/Login.js
@@ -5,6 +5,7 @@
  */
 
 import React, { Component } from 'react';
+import * as firebase from "firebase";
 import {
     StyleSheet,
     Text,
@@ -18,22 +19,54 @@ export default class Login extends Component {
     constructor(props,context){
         super(props,context);
         this.state = {
-            username: "",
-            password: ""
+            email: "",
+            password: "",
+            response: ""
         };
+        this.signup = this.signup.bind(this);
+        this.login = this.login.bind(this);
+    }
+
+    async signup() {
+        try {
+            await firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password);
+            this.setState({
+                response: "account created"
+            });
+            //TODO thanks for signing up page!
+        } catch (error) {
+            this.setState({
+                response: error.toString()
+            })
+        }
+    }
+
+    async login() {
+        try {
+            await firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password);
+            this.setState({
+                response: "Logged In!"
+            });
+            //TODO go to landing page
+        } catch (error) {
+            this.setState({
+                response: error.toString()
+            })
+        }
     }
 
     render() {
         return (
             <View style={styles.container}>
+              <View>
                 <Text style={styles.welcome}>
                     OddZ
                 </Text>
                 <TextInput
                     style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                    onChangeText={(text) => this.setState({username: text})}
+                    onChangeText={(text) => this.setState({email: text})}
                     placeholder="Username"
-                    value={this.state.username}
+                    value={this.state.email}
                 />
                 <TextInput
                     style={{height: 40, borderColor: 'gray', borderWidth: 1}}
@@ -46,13 +79,25 @@ export default class Login extends Component {
                     title="Log In"
                     color="#ffc107"
                     accessibilityLabel="Learn more about this purple button"
+                    onPress={this.login}
+                />
+                <Button
+                    title="Sign Up!"
+                    color="#3b5998"
+                    accessibilityLabel="Learn more about this purple button"
+                    onPress={this.signup}
                 />
                 <Button
                     title="Log In With Facebook"
                     color="#3b5998"
                     accessibilityLabel="Learn more about this purple button"
+                    onPress={null}
                 />
             </View>
+            <View>
+                <Text style={styles.response}>{this.state.response}</Text>
+            </View>
+          </View>
         );
     }
 }
