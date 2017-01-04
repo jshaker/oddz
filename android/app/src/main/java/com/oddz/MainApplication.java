a7a7c576a607bd41034c7c8c35ac712d22c5756b
Mathieu Diab <diabmathieu@gmail.com>
2017-01-04 16:55:44 -0500

    redid whole shit and upgraded rn

--- android/app/src/main/java/com/oddz/MainApplication.java
+++ android/app/src/main/java/com/oddz/MainApplication.java
@@ -1,27 +1,19 @@
 package com.oddz;
 
 import android.app.Application;
+import android.util.Log;
 
-import com.facebook.CallbackManager;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
-
-  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
-
-  protected static CallbackManager getCallbackManager(){
-    return mCallbackManager;
-  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -32,8 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new FBSDKPackage(mCallbackManager)
+          new MainReactPackage()
       );
     }
   };
@@ -46,8 +37,6 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    FacebookSdk.sdkInitialize(getApplicationContext());
-    AppEventsLogger.activateApp(this);
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
