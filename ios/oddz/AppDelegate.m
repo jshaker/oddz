f9d51c083670ce8a78221d62738e77c681fabf9a
Mathieu Diab <diabmathieu@gmail.com>
2017-01-23 14:27:06 -0500

    fid

--- ios/oddz/AppDelegate.m
+++ ios/oddz/AppDelegate.m
@@ -35,20 +35,7 @@
   return YES;
 }
   
-  - (void)applicationDidBecomeActive:(UIApplication *)application {
-    [FBSDKAppEvents activateApp];
-  }
-  
-- (BOOL)application:(UIApplication *)application
-            openURL:(NSURL *)url
-  sourceApplication:(NSString *)sourceApplication
-         annotation:(id)annotation {
-  return [[FBSDKApplicationDelegate sharedInstance] application:application
-                                                        openURL:url
-                                              sourceApplication:sourceApplication
-                                                     annotation:annotation];
-}
-
+ 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     [FBSDKAppEvents activateApp];
   }
