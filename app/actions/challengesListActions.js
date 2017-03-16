8303d92aff961832424746464f3601c078542937
Mathieu Diab <diabmathieu@gmail.com>
2017-03-16 16:26:40 -0400

    mistake while fixing merge conflict

--- app/actions/challengesListActions.js
+++ app/actions/challengesListActions.js
@@ -1,13 +1,11 @@
-
 import {ADD_TO_CHALLENGESLIST, REMOVE_FROM_CHALLENGESLIST} from './actionTypes';
 
 
 export function addToChallengesList(challenge) {
-    return { type: ADD_TO_CHALLENGES_LIST, challenge };
+    return { type: ADD_TO_CHALLENGESLIST, challenge };
 }
 
 export function removeFromChallengesList(challengeID) {
 
     return { type: REMOVE_FROM_CHALLENGESLIST, challengeID };
 }
-
